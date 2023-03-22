2266e97ad836ebd28c9d6b90fd181b36938aa82c
Vasyl <dvorak1981@yandex.ru>
2023-03-22 03:05:24 +0300

    Передача данных между моделью и логикой презентора осуществленна с помощью SwitchMap() RxJava. Взято последнее значение из коллекции данных.

--- app/src/main/java/com/popularlibraries/domain/DataFlowConcatMap.kt
+++ app/src/main/java/com/popularlibraries/domain/DataFlowConcatMap.kt
@@ -8,62 +8,65 @@ import io.reactivex.rxjava3.core.Observable
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
-class DataFlow {
-    fun exec ()  : List<GithubUser>{
-       return Consumer(Producer()).exec()
+class DataFlowConcatMap {
+    fun exec(): List<GithubUser> {
+        return Consumer(Producer()).exec()
     }
+
     class Producer {
-        fun fromCallable () = Observable.fromCallable {
+        fun fromCallable() = Observable.fromCallable {
             val result = GithubUsersRepo().getUsers()
             return@fromCallable result
         }
-
     }
-    class Consumer (val producer: Producer) {
+
+    class Consumer(val producer: Producer) {
         fun execConcatMap(): MutableList<GithubUser> {
             var users = mutableListOf<GithubUser>()
             var finish = true
             producer.fromCallable()
-                .concatMap{              //Последовательная передача данных
-                    val delay = Random.nextInt( 1000).toLong() // Симуляция чтения данных с сервера
-                 it.forEach { user -> user.login = App.instance.applicationContext
-                     .getString(R.string.users_log)+user.login }
-                    return@concatMap Observable.just(it).delay(delay,
-                        TimeUnit.MILLISECONDS)
+                .concatMap {              //Последовательная передача данных
+                    val delay = Random.nextInt(1000).toLong() // Симуляция чтения данных с сервера
+                    it.forEach { user ->
+                        user.login = App.instance.applicationContext
+                            .getString(R.string.users_log) + user.login
+                    }
+                    return@concatMap Observable.just(it)
+                        .delay(delay, TimeUnit.MILLISECONDS)
                 }
                 .subscribe({ s ->
                     users.addAll(s)
 
                 }, {
-                    println( "onError: ${it.message} ")
+                    println("onError: ${it.message} ")
                     finish = false
                 },
                     {
-                     println("Передача данных завершена")
+                        println("Передача данных завершена")
                         finish = false
                     }
-                    )
-            while(finish){
+                )
+
+
+            while (finish) {
                 println("Продолжается передача данных")
             }
+
             return users
         }
 
-
-                fun exec () : List<GithubUser> {
-           return execJust ()
+        fun exec(): List<GithubUser> {
+            return execJust()
         }
 
-
-        fun execJust (): List<GithubUser> {
+        fun execJust(): List<GithubUser> {
             var users = mutableListOf<GithubUser>()
-            producer.fromCallable ()
+            producer.fromCallable()
                 .subscribe {
                     users = execConcatMap()
                 }
 
-
-        return users
+            return users
         }
     }
 }
